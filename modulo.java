44f57e620e1d0a22b184473ed4bdd3e2308a7194
Melvin <mcastrocall@unsa.edu.pe>
2021-04-22 23:13:34 -0500

    Agregando Modulo

---
+++ modulo.java
@@ -0,0 +1,15 @@
+class Calculator{
+	int add(int a, int b){
+		return 0;
+	}
+	int sub(int a,int b){
+		return 0;
+	}
+	int div(int a,int b){
+		return 0;
+	}
+	int mod(int a,int b){
+		int mod=a%b;
+		return mod;
+	}
+}
