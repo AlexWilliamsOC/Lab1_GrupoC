9a37f95758c293de7aa8966e819ec7aacdfb8458
AlexWilliams <aolaechea@unsa.edu.pe>
2021-04-23 00:05:15 -0500

    Se agrego el metodo principal y se corrigieron errores de sintaxis

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 class Calculator {
 	int add(int a, int b) {
 		int sum = a + b;
@@ -16,7 +18,21 @@ class Calculator {
 		return div;
         }
 	int mod(int a, int b) {
-                int mod=a%b
+                int mod=a%b;
                 return mod;
         }
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Calculator c = new Calculator();
+
+		System.out.println("Ingrese dos numeros:");
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		System.out.println("\nAdicion: " + c.add(num1, num2));
+		System.out.println("Sustraccion: " + c.sub(num1, num2));
+		System.out.println("Multiplicacion: " + c.mul(num1, num2));
+		System.out.println("Division: " + c.div(num1, num2));
+		System.out.println("Modulo: " + c.mod(num1, num2));
+	}
 }
