001c6fe3c6bffc0b9fd2479edc969f55d2f28670
AlexWilliams <aolaechea@unsa.edu.pe>
2021-04-22 23:00:48 -0500

    se implemento la funcion mult y div

--- Calculator.java
+++ Calculator.java
@@ -6,10 +6,12 @@ class Calculator {
                 return 0;
         }
 	int mul(int a, int b) {
-                return 0;
+                int mult = a * b;
+		return mult;
         }
 	int div(int a, int b) {
-                return 0;
+                int div = a / b;
+		return div;
         }
 	int mod(int a, int b) {
                 return 0;
